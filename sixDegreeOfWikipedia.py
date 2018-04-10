0f8aefd15f82ba0c5aa012b125c425a1c96980c1
Naruu <esara2021@gmail.com>
2018-04-10 21:03:59 +0900

    crawl all pages

--- sixDegreeOfWikipedia.py
+++ sixDegreeOfWikipedia.py
@@ -1,9 +1,29 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import datetime
+import random
 import re
 
-html = urlopen("http://en.wikipedia.org/wiki/Kevin_Bacon")
-bsObj = BeautifulSoup(html)
-for link in bsObj.fin("div", {"id" : "bodyContent"}).findAll("a", href = re.compile("^(/wiki/)((?!:).)*$")) :
-    if 'href' in link.attrs :
-        print(link.attrs['href'])
+pages = set()
+random.seed(datetime.datetime.now())
+
+def getLinks(articleUrl) :
+    global pages
+    html = urlopen("http://en.wikipedia.org" + articleUrl)
+    bsObj= BeautifulSoup(html)
+    try :
+        print(bsObj.h1.get_text())
+        print(bsObj.find(id = "mw-content-text").findAll("p")[0])
+        print(bsObj.find(id = "ca-eidt").find("span").find("a").attrs["href"])
+    except AttributeError :
+        print("This page is missing something.")
+
+    for link in bsObj.findAll("a", href=re.compile("^(/wiki/)")) :
+        if 'href' in link.attrs :
+            if link.attrs['href'] not in pages :
+                newPage = link.atttrs['href']
+                print("---------\n" + newPage)
+                pages.add(newPage)
+                getLinks(newPage)
+
+getLinks("")
