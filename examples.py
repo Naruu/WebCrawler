e14aeb48953c78fa8b796b9b6385210ea58efdbe
Naruu <esara2021@gmail.com>
2018-04-23 22:53:53 +0900

    find tweets with #python, recent 5 tweets of montypython, post "Hello world"

--- examples.py
+++ examples.py
@@ -9,5 +9,7 @@ pythonTweets = t.search.tweets(q = "#python")
 pythonStatuses = t.statuses.user_timeline(screen_name="montypython", count = 5)
 print(pythonTweets)
 
+## check on https://twitter.com/search first
+
 statusUpdate = t.statuses.update(status = 'Hello, world!')
 print(statusUpdate)
