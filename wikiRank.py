a0b4def9ee296c19b5a660027da512158c2bfdb0
Naruu <esara2021@gmail.com>
2018-04-27 23:42:14 +0900

    typo fixed. Print the locations

--- wikiRank.py
+++ wikiRank.py
@@ -1,5 +1,7 @@
 from urllib.request import urlopen
+from urllib.error   import HTTPError
 from bs4 import BeautifulSoup
+import json
 import datetime
 import random
 import re
@@ -23,12 +25,12 @@ def getHistoryIPs(pageUrl) :
         addressList.add(ipAddress.get_text())
     return addressList
 
-def getConuntry(ipAddress) :
+def getCountry(ipAddress) :
     try :
-        response = urlopen("http ://freegeoip/json/" +ipAddress).read().decode('utf-8')
+        response = urlopen("http://freegeoip.net/json/" +ipAddress).read().decode('utf-8')
 
     except HTTPError :
-        return none
+        return None
     responseJson = json.loads(response)
     return responseJson.get("country_code")
 
@@ -38,6 +40,9 @@ while(len(links) > 0) :
     for link in links :
         historyIPs = getHistoryIPs(link.attrs["href"])
         for historyIP in historyIPs :
+            country = getCountry(historyIP)
+            if country is not None :
+                print(historyIP + "is from " + country)
             print(historyIP)
 
     newLink = links[random.randint(0, len(links) -1)].attrs["href"]
