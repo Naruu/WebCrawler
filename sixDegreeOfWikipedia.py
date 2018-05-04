098b252861bf8611ade97f00184c456d5b746dc7
Naruu <esara2021@gmail.com>
2018-05-04 17:45:42 +0900

    get Bacon number by passsing recursion level

--- sixDegreeOfWikipedia.py
+++ sixDegreeOfWikipedia.py
@@ -1,38 +1,44 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import datetime
-import random
 import re
 import pymysql
 
 conn = pymysql.connect(host= '127.0.0.1', user = 'root', password = '', port=3306, db='mysql', charset='utf8')
 
 cur = conn.cursor()
-cur.execute("USE scraping")
+cur.execute("USE wikipedia")
 
-random.seed(datetime.datetime.now())
+def insertPageIfNotExists(url) :
+    cur.execute("SELECT * FROM pages WHERE url = %s", (url))
+    if cur.rowcount == 0 :
+        cur.execute("INSERT INTO pages (url) VALUES (%s)", (url))
+        conn.commit()
+        return cur.lastrowid
+    else :
+        return cur.fetchone()[0]
 
-def store(title, content) :
-    cur.execute("INSERT INTO pages (title, content) VALUES (\"%s\", \"%s\")", (title, content))
-    cur.connection.commit()
+def insertLink(fromPageId, toPageId) :
+    cur.execute("SELECT * FROM links WHERE fromPageId = %s AND toPageId = %s", (int(fromPageId), int(toPageId)))
+    if cur.rowcount == 0 :
+        cur.execute("INSERT INTO links (fromPageId, toPageId) VALUES (%s, %s)", (int(fromPageId), int(toPageId)))
+        conn.commit()
 
+pages = set()
 
-def getLinks(articleUrl) :
-    html = urlopen("http://en.wikipedia.org" + articleUrl)
+def getLinks(pageUrl, recursionLevel) :
+    global pages
+    if recursionLevel > 4 :
+        return;
+    pageId = insertPageIfNotExists(pageUrl)
+    html = urlopen("http://en.wikipedia.org" + pageUrl)
     bsObj= BeautifulSoup(html)
-    title = bsObj.find("h1").get_text()
-    content = bsObj.find("div",{"id" : "mw-content-text"}).find("p").get_text()
-    store(title, content)
-    return bsObj.find("div", {"id" : "bodyContent"}).findAll("a", href=re.compile("^(/wiki/)((?!:).)*$"))
+    for link in bsObj.findAll("a", href=re.compile("^(/wiki/)((?!:).)*$")):
+        insertLink(pageId, insertPageIfNotExists(link.attrs['href']))
+        if link.attrs['href'] not in pages :
+            newPage = link.attrs['href']
+            pages.add(newPage)
+            getLinks(newPage, recursionLevel+1)
 
-links = getLinks("/wiki/Kevin_Bacon")
-
-try :
-    while len(links)>0 :
-        newArticle = links[random.randint(0,len(links)-1)].attrs["href"]
-        print(newArticle)
-        links = getLinks(newArticle)
-
-finally :
-    cur.close()
-    conn.close()
+getLinks("/wiki/Kevin_Bacon", 0)
+cur.close()
+conn.close()
