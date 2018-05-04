1cf7a404bf9c034e6db3ac0486a45cd7b925a543
Naruu <esara2021@gmail.com>
2018-05-04 17:01:34 +0900

    parse Kevin Bacon page and stoer in mysql

--- sixDegreeOfWikipedia.py
+++ sixDegreeOfWikipedia.py
@@ -3,27 +3,36 @@ from bs4 import BeautifulSoup
 import datetime
 import random
 import re
+import pymysql
 
-pages = set()
+conn = pymysql.connect(host= '127.0.0.1', user = 'root', password = '', port=3306, db='mysql', charset='utf8')
+
+cur = conn.cursor()
+cur.execute("USE scraping")
+
 random.seed(datetime.datetime.now())
 
+def store(title, content) :
+    cur.execute("INSERT INTO pages (title, content) VALUES (\"%s\", \"%s\")", (title, content))
+    cur.connection.commit()
+
+
 def getLinks(articleUrl) :
-    global pages
     html = urlopen("http://en.wikipedia.org" + articleUrl)
     bsObj= BeautifulSoup(html)
-    try :
-        print(bsObj.h1.get_text())
-        print(bsObj.find(id = "mw-content-text").findAll("p")[0])
-        print(bsObj.find(id = "ca-eidt").find("span").find("a").attrs["href"])
-    except AttributeError :
-        print("This page is missing something.")
+    title = bsObj.find("h1").get_text()
+    content = bsObj.find("div",{"id" : "mw-content-text"}).find("p").get_text()
+    store(title, content)
+    return bsObj.find("div", {"id" : "bodyContent"}).findAll("a", href=re.compile("^(/wiki/)((?!:).)*$"))
 
-    for link in bsObj.findAll("a", href=re.compile("^(/wiki/)")) :
-        if 'href' in link.attrs :
-            if link.attrs['href'] not in pages :
-                newPage = link.atttrs['href']
-                print("---------\n" + newPage)
-                pages.add(newPage)
-                getLinks(newPage)
+links = getLinks("/wiki/Kevin_Bacon")
 
-getLinks("")
+try :
+    while len(links)>0 :
+        newArticle = links[random.randint(0,len(links)-1)].attrs["href"]
+        print(newArticle)
+        links = getLinks(newArticle)
+
+finally :
+    cur.close()
+    conn.close()
