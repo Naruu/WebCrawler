405e7331356f2cc3f581913ec8da03e1fb3e11bd
Naruu <esara2021@gmail.com>
2018-05-08 14:49:25 +0900

    text Decoding

--- scrapetest.py
+++ scrapetest.py
@@ -1,11 +1,11 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import re
-from pprint import pprint
 
-html = urlopen("http://www.pythonscraping.com/pages/page3.html")
+textPage = urlopen("http://www.pythonscraping.com/pages/warandpeace/chapter1-ru.txt")
+print(str(textPage.read()), 'utf-8')
+
+html = urlopen("http://www.en.wikipedia.org/wiki/Python_(programming_languages)")
 bsObj = BeautifulSoup(html)
-
-images = bsObj.findAll("img", {"src" : re.compile("\.\.\/img\/gifts/img.*\.jpg")})
-for image in images :
-    print(image["src"])
+content = bsObj.find("div", {"id" : "mw-content-text"}).get_text()
+content = bytes(content, "UTF-8")
+content = content.decode("UTF-8")
