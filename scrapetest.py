c0446779124692890fc69707e199e3e7aec10618
Naruu <esara2021@gmail.com>
2018-05-08 23:35:11 +0900

    use StringIO and read csv file on the web

--- scrapetest.py
+++ scrapetest.py
@@ -1,11 +1,11 @@
 from urllib.request import urlopen
-from bs4 import BeautifulSoup
+from io import StringIO
+import csv
 
-textPage = urlopen("http://www.pythonscraping.com/pages/warandpeace/chapter1-ru.txt")
-print(str(textPage.read()), 'utf-8')
+data = urlopen("http://pythonscraping.com/files/MontyPythonAlbums.csv").read().decode('ascii', 'ignore')
+dataFile = StringIO(data)
+csvReader = csv.reader(dataFile)
 
-html = urlopen("http://www.en.wikipedia.org/wiki/Python_(programming_language)")
-bsObj = BeautifulSoup(html)
-content = bsObj.find("div", {"id" : "mw-content-text"}).get_text()
-content = bytes(content, "UTF-8")
-content = content.decode("UTF-8")
+for row in csvReader :
+    print(row)
+
