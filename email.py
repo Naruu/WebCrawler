715ec582180ad8b1e514f6db1f876e5bc0f209c3
Naruu <esara2021@gmail.com>
2018-05-08 14:01:48 +0900

    check chrsitmas by parsing and send an email if it is a Christmas

--- email.py
+++ email.py
@@ -1,12 +1,21 @@
 import smtplib
 from email.mime.text import MIMEText
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import time
 
-msg = MIMEText("The body of the email is here")
+def sendMail(subject, body) :
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = 'sender@gmail.com'
+    msg['To'] = 'receiver@gmail.com'
 
-msg['Subject'] = 'An email alert'
-msg['From'] = 'sender@gmail.com'
-msg['To'] = 'receiver@gmail.com'
+    s = smtplib.SMTP('localhost')
+    s.send_message(msg)
+    s.quit()
 
-s = smtplib.SMTP('localhost')
-s.send_message(msg)
-s.quit()
+bsObj = BeautifulSoup(urlopen("http://isitchristmas.com/"))
+while(bsObj.find("a", {"id":"answer"}).attrs['title'] == 'NO') :
+    print("It is not Christmas yet.")
+    time.sleep(3600)
+sendMail("It's Christmas", "According to the site. Let's celebrate. Hooray!")
