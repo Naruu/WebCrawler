1072e7ad2e82eef1c326ec7045efbe34ec2bacc0
Naruu <esara2021@gmail.com>
2018-05-08 23:21:39 +0900

    text Decoding

--- scrapetest.py
+++ scrapetest.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 textPage = urlopen("http://www.pythonscraping.com/pages/warandpeace/chapter1-ru.txt")
 print(str(textPage.read()), 'utf-8')
 
-html = urlopen("http://www.en.wikipedia.org/wiki/Python_(programming_languages)")
+html = urlopen("http://www.en.wikipedia.org/wiki/Python_(programming_language)")
 bsObj = BeautifulSoup(html)
 content = bsObj.find("div", {"id" : "mw-content-text"}).get_text()
 content = bytes(content, "UTF-8")
