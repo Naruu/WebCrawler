ce6ea7c4bc9dd80a5f080a2423f49686edf277f5
Naruu <esara2021@gmail.com>
2018-05-09 23:21:30 +0900

    handlie docx,

--- scrapetest.py
+++ scrapetest.py
@@ -8,8 +8,16 @@ wordFile = BytesIO(wordFile)
 document = ZipFile(wordFile)
 xml_content = document.read('word/document.xml')
 
-
 wordObj = BeautifulSoup(xml_content.decode('utf-8'))
 textStrings = wordObj.findAll("w:t")
 for textElem in textStrings :
+    clseTag = ""
+    try :
+        style = textElem.parent.previousSibling.find("w:pstyle")
+        if style is not None and style["w:val"] == "Title" :
+            print("<h1")
+            closeTag = "</h1>"
+    except AttributeError :
+        pass
     print(textElem.text)
+    print(clseTag)
