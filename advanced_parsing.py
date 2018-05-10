c8c2ffc028df6628f7ace828a13c79fe75e9366e
Naruu <esara2021@gmail.com>
2018-05-11 05:35:48 +0900

    isCommon function

--- advanced_parsing.py
+++ advanced_parsing.py
@@ -32,7 +32,16 @@ def ngrams(input, n) :
         output[ngramTemp] += 1
     return output
 
+def isCommon(ngram) :
+    commonWords =["the", "be", "and", "of", "a", "in", "to", "have", "it", "i", "that", "for", "you", "he", "with", "on", "do", "say", "this", "they", "is"," an", "at", "but", "we", "his", "from", "that", "not", "by", "she", "or", "as", "what", "go", "their", "can", "who", "get", "if", "would", "her"", aill", "my", "make", "about", "know", "will", "as", "up", "one", "time", "has", "been", "there", "year",
+ "so", "think", "when", "which", "them", "some", "me", "people", "take", "out", "into", "just", "see", "him", "your", "come", "could", "now", "than", "like", "other", "how", "then", "its", "our", "two", "more", "these", "want", "way", "look", "first", "also", "new", "because", "day", "moe", "use", "no", "man", "find", "here", "thing", "give"]
+
+    for word in ngram :
+        if word in commonWords :
+            return True
+    return False
+
 content = urlopen("http://pythonscraping.com/files/inaugurationSpeech.txt").read().decode('utf-8')
 ngrams = ngrams(content, 2)
-sortedGrams = OrderedDict(ngrams.items(), key = operator.itemgetter(1), reverse =True)
+sortedGrams = sorted(ngrams.items(), key = operator.itemgetter(1), reverse =True)
 print(sortedGrams)
