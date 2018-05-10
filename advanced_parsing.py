7f624ced6d7aae51f25c126e608da0292c08b95c
Naruu <esara2021@gmail.com>
2018-05-10 17:38:42 +0900

    create ngrams

--- advanced_parsing.py
+++ advanced_parsing.py
@@ -1,17 +1,28 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
+import string
+
+def cleanInput(input) :
+    input = re.sub('\n+', " ", input)
+    input = re.sub('\[[0-9]*\]', "", input)
+    input = re.sub(' +', " ", input)
+    input = bytes(input, "UTF-8")
+    input = input.decode("ascii", "ignore")
+    cleanInput= []
+    input = input.split(' ')
+    for item in input :
+        item = item.strip(string.punctuation)
+        if len(item) >1 or (item.lower() == 'a' or item.lower() == 'i') :
+            cleanInput.append(item)
+    return cleanInput
+
 
 def ngrams(input, n) :
-    content = re.sub('\n+', " ", input)
-    content = re.sub(' +', " ", content)
-    content = bytes(content, "UTF-8")
-    content = content.decode("ascii", "ignore")
-    print(content)
-    content = content.split(' ')
-    output =[]
-    for i in range(len(content) -n+1) :
-        output.append(content[i:i+n])
+    input = cleanInput(input)
+    output = []
+    for i in range(len(input) -n+1) :
+        output.append(input[i:i+n])
     return output
 
 html = urlopen("http://en.wikipedia.org/wiki/Python_programming_language")
