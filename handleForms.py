93caa772faf8b089e428247553821b850de31128
Naruu <esara2021@gmail.com>
2018-05-11 22:36:46 +0900

    many kinds of forms

--- handleForms.py
+++ handleForms.py
@@ -1,5 +1,38 @@
 import requests
-
+"""
+##submit forms
 params = {'firstname' : 'Naruu', 'lastname' : 'L'}
 r= requests.post("http://pythonscraping.com/files/processing.php", data=params)
+print(r.text)
+
+##upload images
+files = {'uploadFile' : open('./logo.jpg', 'rb')}
+r=requests.post("http://pythonscraping.com/pages/processing2.php", files=files)
+print(r.text)
+
+## send cookies
+params = {'firstname' : 'Naruu', 'lastname' : 'L'}
+r= requests.post("http://pythonscraping.com/pages/cookies/welcome.php", params)
+print(r.text)
+
+print(r.cookies.get_dict())
+print("----------\n Going to pofile page....")
+r = requests.get("http://pythonscraping.com/pages/cookies/profile.php", cookies=r.cookies)
+print(r.text)
+
+#use sessions
+session = requests.Session()
+
+s = session.post("http://pythonscraping.com/pages/cookies/welcome.php", params)
+print(s.cookies.get_dict())
+
+s =session.get("http://pythonscraping.com/pages/cookies/profile.php")
+"""
+
+##auth
+from requests.auth import AuthBase
+from requests.auth import HTTPBasicAuth
+
+auth = HTTPBasicAuth('Naruu', 'password')
+r = requests.post(url = "http://pythonscraping.com/pages/auth/login.php", auth = auth)
 print(r.text)
