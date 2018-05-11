40b34519ac03d8dc3438a797300f1dde43e439bb
Naruu <esara2021@gmail.com>
2018-05-11 21:51:59 +0900

    pos_tag

--- handleNLTK.py
+++ handleNLTK.py
@@ -1,7 +1,5 @@
 from nltk.book import text6
-from nltk import FreqDist
-from nltk import bigrams
-
-bigrams = bigrams(text6)
-bigramDist=FreqDist(bigrams)
-bigramDist[("sir", "Robin")]
+from nltk import word_tokenize, pos_tag
+text = word_tokenize("strange women lying in ponds distributing swords is no basis for a system of government. Supreme executive power deries from a mandate from the masses, not from some farcical aquatic ceremony.")
+p = pos_tag(text)
+print(p)
