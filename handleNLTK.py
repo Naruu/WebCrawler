f4a51235188d9a4f26d729b8c86ef00147a64dfd
Naruu <esara2021@gmail.com>
2018-05-11 21:55:09 +0900

    pos_tag usage

--- handleNLTK.py
+++ handleNLTK.py
@@ -1,5 +1,10 @@
-from nltk.book import text6
-from nltk import word_tokenize, pos_tag
-text = word_tokenize("strange women lying in ponds distributing swords is no basis for a system of government. Supreme executive power deries from a mandate from the masses, not from some farcical aquatic ceremony.")
-p = pos_tag(text)
-print(p)
+from nltk import word_tokenize, sent_tokenize, pos_tag
+sentences = sent_tokenize("Google is one of the best companies in the world. I constantly google myself to see what I'm up to.")
+nouns = ['NN', 'NNS', 'NNP', 'NNPS']
+
+for sentence in sentences :
+    if "google" in sentence.lower() :
+        taggedWords = pos_tag(word_tokenize(sentence))
+        for word in taggedWords :
+            if word[0].lower() == "google" and word[1] in nouns :
+                print(sentence)
