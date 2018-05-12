4e38bc2f3617260707eb9e49072a0fb7247f0189
Naruu <esara2021@gmail.com>
2018-05-13 03:24:23 +0900

    check the page change

--- seleniumForJS.py
+++ seleniumForJS.py
@@ -1,11 +1,23 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.common.exceptions import StaleElementReferenceException
+import time
 
-driver = webdriver.PhantomJS(executeable_path='')
-driver.get("http://pythonscraping.com/pages/javascript/ajaxDemo.html")
-try :
-    element = WebDriverWait(driver, 10).until(EC.presence _of_element_located(By.ID, "loadedButton"))
-finally :
-    print(driver.find_element_by_id("content").text)
-    driver.close()
+def waitForLoad(driver) :
+    elem = driver.find_element_by_tag_name('html')
+    count = 0
+    while True :
+        count = count + 1
+        if count > 20 :
+            print("Timing out after 10 seconds and returning")
+            return
+        time.sleep(.5)
+        try :
+            elem = driver.find_elemnt_by_tag_name('html')
+        except StaleElementReferenceException :
+            return
+
+driver = webdriver.PhantomJS(executable_path = '')
+driver.get("http://pythonscraping.com/pages/javascript/redirectDemo1.html")
+waitForLoad(driver)
+print(driver.page_source)
