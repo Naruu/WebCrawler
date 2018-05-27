7166f7d2bb5fdfa48ddc5883ad800b4662d6976e
Naruu <esara2021@gmail.com>
2018-05-27 15:45:06 +0900

    parse the average temperature from Korean Meteological Administration site and plot need to show the unit of x and y

---
+++ getAvTemp.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup, NavigableString
+from urllib.request import urlopen
+import matplotlib.pyplot as plt
+
+html = urlopen("http://www.weather.go.kr/weather/climate/past_table.jsp?stn=108&yy=2018&obs=07&x=21&y=8")
+bsObj = BeautifulSoup(html)
+table = bsObj.find("div", {'id': 'content_weather'}).find("tbody")
+day = range(1,32)
+temp = []
+for tr in table :
+    if isinstance(tr, NavigableString) :
+        pass
+    else :
+        temp.append(float(tr.contents[3].get_text()))
+
+plt.style.use('fivethirtyeight')
+plt.plot(day, temp[:31])
+plt.title('2018.01. Average Temperature of Seoul')
+plt.show()
+
+#x, y axis 단위 적기!
